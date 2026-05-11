25058142c0e42ff4c8edcd52c1a45e8dd26fda0e
Ife <morakinyoifeoluwa55@gmail.com>
2026-05-11 08:15:25 +0100

    i was able to update the router auth.js to able to get te verify email

--- router/auth.js
+++ router/auth.js
@@ -3,9 +3,9 @@
 const express = require('express')
   const router = express.Router()
 
-const {signup,signin} = require('../controller/auth')
+const {signup,signin,verifyEmail} = require('../controller/auth')
 
-
+router.route('/verify-email').get(verifyEmail)
 router.route('/signup').post( signup)
 router.route('/signin').post( signin)
 
