dbe74ce264d5d743dc0d36b73565812e729a60d7
Ife <morakinyoifeoluwa55@gmail.com>
2026-05-16 04:58:37 +0100

    Add error handling

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose')
 
-
-
-
 const connectDB = (url) => {
-  return mongoose.connect(url);
-};
+  return mongoose.connect(url)
+    .then(() => {
+      console.log('MongoDB connected successfully!')
+    })
+    .catch((err) => {
+      console.log(' MongoDB connection failed:', err.message)
+      throw err
+    })
+}
 
-module.exports = connectDB;
-
-
+module.exports = connectDB
