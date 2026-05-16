0f224b175826dd58eb91d8823ec46329ec924091
Ife <morakinyoifeoluwa55@gmail.com>
2026-05-16 05:10:26 +0100

    Deploy with MongoDB IP whitelist fixed

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = (url) => {
   return mongoose.connect(url)
     .then(() => {
-      console.log('MongoDB connected successfully!')
+      console.log('MongoDB connected successfully,your mongo URI is correct')
     })
     .catch((err) => {
       console.log(' MongoDB connection failed:', err.message)
