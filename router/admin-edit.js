af19b44663e20d917476cbf2b907cf1cc3f4e9fd
Ife <morakinyoifeoluwa55@gmail.com>
2026-06-03 22:45:28 +0100

    update admin-edit router

--- router/admin-edit.js
+++ router/admin-edit.js
@@ -1,20 +1,15 @@
 
+
 const express = require('express')
 const router = express.Router()
 const authMiddleware = require('../middleware/auth')
-const {
-getSingleUser,
-updateUser
-} = require('../controller/admin-edit')
+const { getSingleUser, updateUser } = require('../controller/admin-edit')
+const upload = require('../middleware/multer-middleware')
 
+// GET single user
+router.get('/user/:id', authMiddleware, getSingleUser)
 
-
-
-
-
-
-
-router.patch('/users/:id', authMiddleware, updateUser)
-router.get('/users/:id', authMiddleware, getSingleUser)
+// UPDATE user (with image upload)
+router.put('/user/:id', upload.single('profilePicture'), updateUser)
 
 module.exports = router
